e6a418a4eae7690b505ff9963063db6f8f736b2f
ekank <ekankr2@naver.com>
2021-08-28 13:40:17 +0900

    table for selecting city

--- frontend/src/views/TestPage.vue
+++ frontend/src/views/TestPage.vue
@@ -2,38 +2,23 @@
   <div id="app">
     <v-app id="inspire">
       <v-card class="mx-auto" max-width="400">
-        <v-card-title class="white--text orange darken-4">
+        <v-card-title class="white--text text-h5 text font-weight-medium orange darken-4">
           Select City
 
           <v-spacer></v-spacer>
 
-          <v-btn color="white"
-              class="text--primary" fab small>
+          <v-btn color="white" class="text--primary" @click="show = !show" fab small>
             <v-icon>mdi-plus</v-icon>
           </v-btn>
         </v-card-title>
         <v-divider></v-divider>
 
-        <v-card elevation="16" max-width="400" min-width="400" class="mx-auto">
-          <v-simple-table height="300px">
+        <v-card elevation="10" max-width="300" min-width="300" class="mx-auto">
+          <v-simple-table height="300px" v-if="show">
             <template v-slot:default>
-              <thead>
-              <tr>
-                <th class="text-left">
-                  Name
-                </th>
-                <th class="text-left">
-                  Calories
-                </th>
-              </tr>
-              </thead>
               <tbody>
-              <tr
-                  v-for="item in desserts"
-                  :key="item.name"
-              >
-                <td>{{ item.name }}</td>
-                <td>{{ item.calories }}</td>
+              <tr v-for="item in data" :key="item.city">
+                <td class="text-h6 font-weight-regular">{{item.city}}, {{item.country}}</td>
               </tr>
               </tbody>
             </template>
@@ -50,51 +35,11 @@ export default {
   name: "TestPage",
   data() {
     return {
-      desserts: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-        },
-      ],
+      show: false,
+      data: this.$store.getters.walkVid,
     }
   },
-  computed: {
+  methods: {
 
   }
 }

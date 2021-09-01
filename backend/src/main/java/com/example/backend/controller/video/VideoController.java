e7554fd989dcbc9772833aea09d2f1339614440b
ekank <ekankr2@naver.com>
2021-09-01 22:15:18 +0900

    Video Request DTO

---
+++ backend/src/main/java/com/example/backend/controller/video/VideoController.java
@@ -0,0 +1,4 @@
+package com.example.backend.controller.video;
+
+public class VideoController {
+}
--- frontend/src/components/WalkContents/WalkSelectBox.vue
+++ frontend/src/components/WalkContents/WalkSelectBox.vue
@@ -46,7 +46,6 @@ export default {
       selectedPlace: {},
     }
   },
-  props: ['value'],
   computed: {
     ...mapState(['walk','selectedWalk'])
   },
